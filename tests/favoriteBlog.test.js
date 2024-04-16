438a483645aae2ef5ced1229fe2c39bc31a48ddd
saidam90 <saida.murtazaliyeva@yahoo.com>
2024-04-16 19:56:17 +0400

    Solution for 4.5*: Helper Functions and Unit Tests, step 3

--- tests/favoriteBlog.test.js
+++ tests/favoriteBlog.test.js
@@ -2,18 +2,7 @@ const { test, describe } = require("node:test");
 const assert = require("node:assert");
 const listHelper = require("../utils/list_helper");
 
-describe("total likes", () => {
-  const listWithOneBlog = [
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 5,
-      __v: 0,
-    },
-  ];
-
+describe("favoriteBlog", () => {
   const blogs = [
     {
       _id: "5a422a851b54a676234d17f7",
@@ -65,13 +54,8 @@ describe("total likes", () => {
     },
   ];
 
-  test("when list has only one blog, equals the likes of that", () => {
-    const result = listHelper.totalLikes(listWithOneBlog);
-    assert.strictEqual(result, 5);
-  });
-
-  test("number of likes in blogs", () => {
-    const result = listHelper.totalLikes(blogs);
-    assert.strictEqual(result, 36);
+  test("the favorite blog", () => {
+    const result = listHelper.favoriteBlog(blogs);
+    assert.deepStrictEqual(result, 2);
   });
 });
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,16 +2,32 @@ const dummy = (blogs) => {
   return 1;
 };
 
-const totalLikes = (listWithBlogs) => {
+const totalLikes = (blogs) => {
   total = 0;
-  for (i = 0; i < listWithBlogs.length; i++) {
-    total += listWithBlogs[i].likes;
+  for (i = 0; i < blogs.length; i++) {
+    total += blogs[i].likes;
   }
   // console.log(total);
   return total;
 };
 
+const favoriteBlog = (blogs) => {
+  let likes = [];
+  for (i = 0; i < blogs.length; i++) {
+    likes.push(blogs[i].likes);
+  }
+  const favorite = Math.max(...likes);
+
+  for (i = 0; i < blogs.length; i++) {
+    if (favorite === blogs[i].likes) {
+      return blogs.indexOf(blogs[i]);
+      // console.log(blogs.indexOf(blogs[i]));
+    }
+  }
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
