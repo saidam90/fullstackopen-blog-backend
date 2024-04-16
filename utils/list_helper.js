b950c0459c1481e2d48f566466fd2c94e0bf2fbd
saidam90 <saida.murtazaliyeva@yahoo.com>
2024-04-16 19:14:24 +0400

    Solution for 4.4: Helper Functions and Unit Tests, step 2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (listWithBlogs) => {
+  total = 0;
+  for (i = 0; i < listWithBlogs.length; i++) {
+    total += listWithBlogs[i].likes;
+  }
+  // console.log(total);
+  return total;
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
